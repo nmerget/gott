a6cb681836151dd80704a94d386c7cad964f7767
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-10-10 23:08:28 +0200

    test: add resource test

--- src/test.tsx
+++ src/test.tsx
@@ -1,14 +1,18 @@
 import npm from "./npm-modules";
-import { signal } from "./decorators";
+import { property, signal } from "./decorators";
 import Bla from "res://scripts/gd/Bla.gd";
 import ReadTest from "./readTest";
+import ResourceTest from "./resourceTest";
 
 export default class Test extends godot.Node {
-  // define a signal
   @signal
   static readonly onReady: string;
 
   label: godot.Label;
+
+  @property({ type: godot.VariantType.TYPE_OBJECT })
+  public stats: ResourceTest;
+
   _ready(): void {
     this.label = this.get_node(godot.Label);
     this.emit_signal(Test.onReady);
@@ -16,6 +20,11 @@ export default class Test extends godot.Node {
     bla.call("run_in_bla");
 
     ReadTest.singleton.logTestType();
+
+    if (this.stats) {
+      this.stats.init(10);
+      console.log(this.stats.health);
+    }
   }
 
   changeTextInLabel = () => {
